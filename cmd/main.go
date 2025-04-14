aabbc1d1b887d9f00a639dd1425191068de54248
Antoine Debroye <ant.debroye@gmail.com>
2025-04-14 21:07:21 +0200

    feat: adding routes to the main

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"github.com/DebroyeAntoine/go_link_vault/internal/db"
-	"github.com/DebroyeAntoine/go_link_vault/internal/link"
+	"github.com/DebroyeAntoine/go_link_vault/internal/handler"
+	"github.com/DebroyeAntoine/go_link_vault/internal/middleware"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,7 +13,10 @@ func main() {
 
 	r := gin.Default()
 
-	r.POST("/links", link.CreateLinkHandler)
+	r.POST("/register", handler.RegisterUserHandler)
+	r.POST("/links", handler.LoginUserHandler)
+	r.POST("/links", middleware.AuthRequired(), handler.CreateLinkHandler)
+	r.GET("/links", middleware.AuthRequired(), handler.GetLinksHandler)
 
 	r.Run(":8080")
 }
